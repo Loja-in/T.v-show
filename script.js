68f0c8e55eb5b99c0dc60a4c5358e9b18acb6acc
lojain <lojainnasr039@gmail.com>
2024-01-28 18:20:38 +0300

    Add watch button functionality, add a sign up form, style the sign up form.

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('watch').addEventListener('click', function() {
+        window.open('https://www.hulu.com/series/house-ef39603f-eb90-424  8-8237-f6168d7c1be1', '_blank');
+    });
