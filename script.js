f59f1a15405f992de822b6ea1779936184372bf5
lojain <lojainnasr039@gmail.com>
2024-01-28 19:27:39 +0300

    Update HTML, CSS, and JS files. Add event listeners and functionality for sign-up form.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('watch').addEventListener('click', function() {
         window.open('https://www.hulu.com/series/house-ef39603f-eb90-424  8-8237-f6168d7c1be1', '_blank');
     });
+
+    document.getElementById("signup-link").addEventListener("click", function (event) {
+        event.preventDefault();
+        document.getElementById("container").style.display = "none";
+        document.getElementById("sign-up").style.display = "block";
+        
+
+    });
